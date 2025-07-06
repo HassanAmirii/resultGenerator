c9b7d395e4c330d7150a1b642b6cff8df08cb5b6
Hassan Amiri <hassanamiri.ai@gmail.com>
2025-07-06 17:25:45 +0100

    Refactor input form and update result fetching logic in index.html and index.js

--- script/index.js
+++ script/index.js
@@ -4,15 +4,29 @@ document.addEventListener("DOMContentLoaded", () => {
 2. fetch result from json
 3. display in app.innerHTML 
 */
+
+  //   const inputBar = document.getElementsById("inputBar");
+  //   const resultTemplate = document.getElementById("resultTemplate");
+  const getResult = inputBar.innerHTML;
   fetch("/data/result.json")
     .then((res) => {
       if (!res.ok) {
         throw new Error(`http error: ${res.status}`);
       }
+      return res.json();
     })
 
     .then((data) => {
-      console.log(data);
+      const getDataObj = data.results;
+      console.log(getDataObj);
+      const getDesiredResult = getDataObj.find(function (resultItem) {
+        return resultItem.getResult;
+      });
+      console.log(getDesiredResult);
+
+      //   const getResultObj = getDataObj[getResult];
+
+      //   resultTemplate.innerHTML = `<p>your results : ${getDataObj[getResult]}</p>`;
     })
 
     .catch((error) => {

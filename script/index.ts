3d94910c92468f47c1546a2dbd10324aa56628f4
Hassan Amiri <hassanamiri.ai@gmail.com>
2025-07-08 12:46:39 +0100

    Refactor index.ts to enhance data structure definitions for student results

--- script/index.ts
+++ script/index.ts
@@ -1,8 +1,19 @@
 // blueprint of the json data
 
+// first; the surface level key types
 interface studentsResults {
   name: string;
   class: string;
   term: string;
   pdf: string;
+}
+
+// second; the key of the whole surface surface level
+interface allStudentsResultsMap {
+  [key: string]: studentsResults;
+}
+
+// third; overall/top level type
+interface fetchJsonData {
+  results: allStudentsResultsMap[];
 }

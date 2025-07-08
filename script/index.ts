c14352a43125d98c72e59b245ef4ecf530fb8021
Hassan Amiri <hassanamiri.ai@gmail.com>
2025-07-08 12:38:11 +0100

    Rename studentsInfo interface to studentsResults and update related interfaces for consistency

--- script/index.ts
+++ script/index.ts
@@ -1,6 +1,14 @@
-interface studentsInfo {
+interface studentsResults {
   name: string;
   class: string;
   term: string;
   pdf: string;
+}
+
+interface allStudentResultMap {
+  [key: string]: studentsResults;
+}
+
+interface fetchJSonData {
+  results: allStudentResultMap[];
 }
